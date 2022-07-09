7cc79a29e9c28b863e388956cc1bc3d23f285051
hoaveth <hoaveth.gaming@gmail.com>
2022-07-09 23:04:26 +0800

    [Update] - Added a signup component for the project

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,7 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./Signup.css";
 
 export default function Signup() {
-  return <div>Signup</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [displayName, setDisplayName] = useState("");
+  const [thumbnail, setThumbnail] = useState(null);
+
+  return (
+    <form className="auth-form">
+      <h2>Welcome to The Dojo.</h2>
+      <h4 className="header-label">Create your account by signing up.</h4>
+      <label>
+        <span>Email:</span>
+        <input
+          type="email"
+          required
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+      <label>
+        <span>Password:</span>
+        <input
+          type="password"
+          required
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+      <label>
+        <span>Display Name:</span>
+        <input
+          type="text"
+          required
+          onChange={(e) => setDisplayName(e.target.value)}
+          value={displayName}
+        />
+      </label>
+      <label>
+        <span>Avatar:</span>
+        <input type="file" required />
+      </label>
+      <button className="btn">Sign Up</button>
+    </form>
+  );
 }
